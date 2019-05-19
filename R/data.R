514865dab4dd8888a0087f908759eb6e80edd6e7
Jaimemosg <jmosquerag@unal.edu.co>
2019-05-19 13:58:47 -0500

    Data from paper added.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Tensile strengths
+#'
+#' Tensile strengths (in GPa) of 69 specimens of carbon fiber tested under tension
+#' at gauge lengths of 20 mm.
+#'
+#' @format A data frame with 69 observations.
+"Fibers"
