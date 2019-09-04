70f367bf987d7c4f895c572c0f635784529c97ee
Jaime Mosquera Gutiérrez <37384444+Jaimemosg@users.noreply.github.com>
2019-09-04 16:04:05 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 #Este es un mensaje de prueba (Jaime Mosquera)
 .onAttach <- function(libname, pkgname){
-  initmessage1 <- "<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version"
-  initmessage2 <- utils::packageDescription("EstimationTools ")$Version
+  initmessage1 <- "<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version "
+  initmessage2 <- utils::packageDescription("EstimationTools")$Version
   initmessage3 <- ">>>>>>>>>>>>>>>>>>>>>
   Feel free to report bugs in https://github.com/Jaimemosg/EstimationTools/issues"
   packageStartupMessage(paste0(initmessage1, initmessage2, initmessage3))
