a2049725f91078795b4a3552bb5c8d2e5da0c8ec
Jaimemosg <jmosquerag@unal.edu.co>
2019-10-29 14:40:11 +0000

    Updated.

--- vignettes/Bootstrap_implementation.R
+++ vignettes/Bootstrap_implementation.R
@@ -8,35 +8,21 @@ knitr::opts_chunk$set(
 #  if (!require('devtools')) install.packages('devtools')
 #  devtools::install_github('Jaimemosg/EstimationTools', force = TRUE)
 
-## ----warning=FALSE, message=FALSE----------------------------------------
-library(EstimationTools)
+## ------------------------------------------------------------------------
+norm_reg2 <- maxlogLreg(formulas, y_dist = y ~ dnorm,
+                        link = list(over = "sd", fun = "log_link"))
 
-x <- rnorm(n = 10000, mean = 160, sd = 6)
-theta_1 <- maxlogL(x = x, dist = 'dnorm', control = list(trace = 1),
-                   link = list(over = "sd", fun = "log_link"),
-                   fixed = list(mean = 160))
-summary(theta_1)
-
-## Hessian 
-print(theta_1$fit$hessian)
-
-## Standard errors
-print(theta_1$outputs$StdE)
-print(theta_1$outputs$StdE_Method)
-
-## ----echo=FALSE----------------------------------------------------------
-a <- theta_1$outputs$StdE
+# Bootstrap for regression model
+summary(norm_reg2, Boot_Std_Err = TRUE, R = 1000)
 
 ## ------------------------------------------------------------------------
-summary(theta_1, Boot_Std_Err = TRUE, R = 1000)
-
-## Hessian 
-print(theta_1$fit$hessian)
+## Hessian
+print(norm_reg2$fit$hessian)
 
 ## Standard errors
-print(theta_1$outputs$StdE)
-print(theta_1$outputs$StdE_Method)
+print(norm_reg2$outputs$StdE)
+print(norm_reg2$outputs$StdE_Method)
 
 ## ----echo=FALSE----------------------------------------------------------
-b <- theta_1$outputs$StdE
+e <- norm_reg2$outputs$StdE
 
