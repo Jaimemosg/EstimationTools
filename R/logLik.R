09c395cd461876045a8d9621216a6f6fb62f2da3
Jaimemosg <jmosquerag@unal.edu.co>
2019-11-01 00:09:44 +0000

    logLik method.

---
+++ R/logLik.R
@@ -0,0 +1,13 @@
+logLik <- function(object, ...) UseMethod("logLik")
+
+logLik.maxlogL <- function(object, ...){
+  p <- ifelse(object$outputs$type == "maxlogL",
+              object$outputs$npar,
+              sum(as.numeric(object$outputs$b_length)))
+  val <- object$fit$objective
+  attr(val, "nall") <- object$outputs$n
+  attr(val, "nobs") <- object$outputs$n
+  attr(val, "df") <- p
+  class(val) <- "logLik"
+  val
+}
