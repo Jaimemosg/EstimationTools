2628042a0a8d5a0be2db53f20194711668c7ec0d
Jaimemosg <jmosquerag@unal.edu.co>
2019-12-05 19:52:48 +0000

    New script with generic functions.

---
+++ R/generic_funcs_maxlogL.R
@@ -0,0 +1,21 @@
+#==============================================================================
+# logLik method ---------------------------------------------------------------
+#==============================================================================
+#' @export
+logLik.maxlogL <- function(object, ...){
+  p <- ifelse(object$outputs$type == "maxlogL",
+              object$outputs$npar,
+              sum(as.numeric(object$outputs$b_length)))
+  val <- object$fit$objective
+  attr(val, "nall") <- object$outputs$n
+  attr(val, "nobs") <- object$outputs$n
+  attr(val, "df") <- p
+  class(val) <- "logLik"
+  val
+}
+#==============================================================================
+# maxlogL function identification ---------------------------------------------
+#==============================================================================
+is.maxlogL <- function(x){
+  inherits(x, c("maxlogL"))
+}
