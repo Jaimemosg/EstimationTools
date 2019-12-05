c339b28823670559affb8a3b197d717cc2a3d246
Jaimemosg <jmosquerag@unal.edu.co>
2019-12-05 19:52:29 +0000

    Replaced in other script.

--- R/summary.R
+++ R/summary.R
@@ -333,18 +333,3 @@ print.maxlogL <- function(x, ...) {
     print(result)
   }
 }
-#==============================================================================
-# logLik method ---------------------------------------------------------------
-#==============================================================================
-#' @export
-logLik.maxlogL <- function(object, ...){
-  p <- ifelse(object$outputs$type == "maxlogL",
-              object$outputs$npar,
-              sum(as.numeric(object$outputs$b_length)))
-  val <- object$fit$objective
-  attr(val, "nall") <- object$outputs$n
-  attr(val, "nobs") <- object$outputs$n
-  attr(val, "df") <- p
-  class(val) <- "logLik"
-  val
-}
