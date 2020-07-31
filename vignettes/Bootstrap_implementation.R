82461ba83da69a1c41c73961c2a9c00e1cd56101
Jaimemosg <jmosquerag@unal.edu.co>
2020-07-31 15:09:22 -0500

    Updated,

--- vignettes/Bootstrap_implementation.R
+++ vignettes/Bootstrap_implementation.R
@@ -1,14 +1,14 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- eval=FALSE---------------------------------------------------------
+## ----install, eval=FALSE------------------------------------------------------
 #  if (!require('devtools')) install.packages('devtools')
 #  devtools::install_github('Jaimemosg/EstimationTools', force = TRUE)
 
-## ----warning=FALSE, message=FALSE----------------------------------------
+## ----HessianOptim, warning=FALSE, message=FALSE-------------------------------
 library(EstimationTools)
 
 x <- rnorm(n = 10000, mean = 160, sd = 6)
@@ -21,26 +21,27 @@ summary(theta_1)
 print(theta_1$fit$hessian)
 
 ## Standard errors
-print(theta_1$outputs$StdE)
+print(theta_1$fit$StdE)
 print(theta_1$outputs$StdE_Method)
 
-## ----echo=FALSE----------------------------------------------------------
-a <- theta_1$outputs$StdE
+## ----Hessian1, echo=FALSE-----------------------------------------------------
+a <- theta_1$fit$StdE
 
-## ------------------------------------------------------------------------
+## ----HessianBootstrap---------------------------------------------------------
 # Bootstrap
 theta_2 <- maxlogL(x = x, dist = 'dnorm', control = list(trace = 1),
                    link = list(over = "sd", fun = "log_link"),
                    fixed = list(mean = 160))
-summary(theta_2, Boot_Std_Err = TRUE, R = 1000)
+bootstrap_maxlogL(theta_2, R = 200)
+summary(theta_2)
 
 ## Hessian
 print(theta_2$fit$hessian)
 
 ## Standard errors
-print(theta_2$outputs$StdE)
+print(theta_2$fit$StdE)
 print(theta_2$outputs$StdE_Method)
 
-## ----echo=FALSE----------------------------------------------------------
-b <- theta_2$outputs$StdE
+## ----Hessian2, echo=FALSE-----------------------------------------------------
+b <- theta_2$fit$StdE
 
