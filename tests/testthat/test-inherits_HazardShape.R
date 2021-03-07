5c84308dd98e43bbc6c1547ee88c99dc4ddad264
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-07 01:11:46 -0500

    Updated.

--- tests/testthat/test-inherits_HazardShape.R
+++ tests/testthat/test-inherits_HazardShape.R
@@ -1,9 +1,7 @@
-context("inherits method for HazardShape")
-
 test_that("running 'TTT_hazard_shape'", {
 
   y <- rweibull(n = 50, shape = 2.5, scale = pi)
-  my_initial_guess <- TTT_hazard_shape(formula = y ~ 1)
-  expect_equal( is.HazardShape(my_initial_guess), "HazardShape")
+  my_initial_guess <- TTT_hazard_shape(formula = y ~ 1, silent = TRUE)
+  expect_equal( is.HazardShape(my_initial_guess), TRUE )
 
 })
