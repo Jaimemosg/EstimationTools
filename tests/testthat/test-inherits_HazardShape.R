afa97d2536ece94b6b13969627f014f5037d2b6d
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-07 00:08:23 -0500

    Correction.

--- tests/testthat/test-inherits_HazardShape.R
+++ tests/testthat/test-inherits_HazardShape.R
@@ -1,6 +1,6 @@
 context("inherits method for HazardShape")
 
-test_that("running mean with constant x or position", {
+test_that("running 'TTT_hazard_shape'", {
 
   y <- rweibull(n = 50, shape = 2.5, scale = pi)
   my_initial_guess <- TTT_hazard_shape(formula = y ~ 1)
