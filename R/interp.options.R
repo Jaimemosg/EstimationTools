7c0fb83bb5c0d14d77d4b36128b5c88856dcbd4b
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-06 23:02:12 -0500

    Creation of extra methods and Spelling check.

---
+++ R/interp.options.R
@@ -0,0 +1,47 @@
+#' Configure various aspects of interpolating function in \code{TTT_hazard_shape}
+#'
+#' @author Jaime Mosquera Gutiérrez \email{jmosquerag@unal.edu.co}
+#'
+#' @description
+#' This function allows the user to set the parameters of any of the following
+#' interpolating functions which can be used inside \code{\link{TTT_hazard_shape}}.
+#'
+#' @param interp.fun character. This argument defines the interpolating
+#'                   function used. Default value is
+#'                   \code{"splinefun"}. Visit the
+#'                   \strong{Details} section for further information.
+#' @param length.out numeric. Number of points interpolated. Default value is 10.
+#' @param ... further arguments passed to the interpolating function function.
+#'
+#' @details
+#' Each interpolating function has its particular arguments. The following
+#' interpolating functions are  recommended:
+#'
+#' \itemize{
+#' \item \code{\link[stats]{approxfun}}
+#' \item \code{\link[stats]{splinefun}}
+#' }
+#'
+#' The user can also implement a custom interpolating function.
+#'
+#' @seealso \code{\link[stats]{approxfun}}, \code{\link[stats]{splinefun}},
+#' \code{\link[stats]{smooth}}, \code{\link[stats]{smooth.spline}},
+#' \code{\link[stats]{loess}}, \code{\link{TTT_hazard_shape}}
+#' @export
+interp.options <- function(interp.fun = "splinefun", length.out = 10, ...){
+  dots <- substitute(...())
+  names_dots <- names(dots)
+  dots_match <- match(c("x", "y"), names_dots)
+  if ( any(!is.na(dots_match)) ){
+    dots_match <- na.omit(dots_match)
+    forbidden <- names_dots[dots_match]
+    sentence <- c(" Argument ", " is ")
+    if ( length(forbidden) > 1 ) sentence <- c("Arguments ", " are ")
+    stop(paste0(sentence[1], forbidden, sentence[2], "forbidden ", "for",
+                " 'loess.options' function."))
+  }
+  list_out <- list(interp.fun = interp.fun, length.out = length.out,
+                   passing_args = list(...))
+  if ( length(list_out$passing_args) == 0 ) list_out$passing_args <- NULL
+  return(list_out)
+}
---
+++ R/loess.options.R
@@ -0,0 +1,37 @@
+#' Configure various aspects of LOESS in \code{TTT_hazard_shape}
+#'
+#' @author Jaime Mosquera Gutiérrez \email{jmosquerag@unal.edu.co}
+#'
+#' @description
+#' This function allows the user to set the parameters of \code{loess} function
+#' used inside \code{TTT_hazard_shape}.
+#'
+#' @param span 	the parameter which controls the degree of smoothing.
+#' @param ... further arguments passed to \code{\link[stats]{loess}} function.
+#'
+#' @details
+#' Please, visit \code{\link[stats]{loess}} to know further possible arguments.
+#' The following arguments are not available for passing to the LOESS estimation:
+#' \itemize{
+#' \item{data}{The only data handled inside \code{TTT_hazard_shape} is the
+#' computed empirical TTT.}
+#' \item{subset}{This argument is used in \code{loess} to take a subset of data.
+#' In this context, it is not necessary.}
+#' }
+#'
+#' @seealso \code{\link[stats]{loess}}, \code{\link{TTT_hazard_shape}}
+#' @export
+loess.options <- function(span = 2/3, ...){
+  dots <- substitute(...())
+  names_dots <- names(dots)
+  dots_match <- match(c("data", "subset"), names_dots)
+  if ( any(!is.na(dots_match)) ){
+    dots_match <- na.omit(dots_match)
+    forbidden <- names_dots[dots_match]
+    sentence <- c(" Argument ", " is ")
+    if ( length(forbidden) > 1 ) sentence <- c("Arguments ", " are ")
+    stop(paste0(sentence[1], forbidden, sentence[2], "forbidden ", "for",
+                " 'loess.options' function."))
+  }
+  return(list(span=span, ...))
+}
