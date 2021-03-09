26bd986f01dfcda990d9021e4d6ddf9e3feb6f1d
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-08 19:16:42 -0500

    Extra line dropped.

--- tests/testthat/test-interp_options.R
+++ tests/testthat/test-interp_options.R
@@ -5,7 +5,6 @@ test_that("interp.options test", {
   y <- rweibull(n = 50, shape = 2.5, scale = pi)
   my_initial_guess <- TTT_hazard_shape(formula = y ~ 1,
                                        interpolation = myoptions)
-  my_initial_guess$hazard_type
   expect_equal( my_initial_guess$hazard_type, "Increasing" )
 })
 
