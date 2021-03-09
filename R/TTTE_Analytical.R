5d2a8ea6e820c22b0fc5e4280e6028210bb10541
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-09 11:35:38 -0500

    Example changed.

--- R/TTTE_Analytical.R
+++ R/TTTE_Analytical.R
@@ -109,40 +109,12 @@
 #' print(TTT_4$strata)
 #'
 #' #--------------------------------------------------------------------------------
-#' # Eample 5: empirical TTT with a continuously variant term for the the shape
-#' # parameter in Odd Weibull distribution.
+#' # Eample 5: empirical TTT with a continuously variant term for the shape
+#' # parameter in Weibull distribution.
 #'
-#' qOW <- function(p, mu, sigma, nu, lower.tail=TRUE, log.p = FALSE){
-#' if (any(mu<=0 ))
-#'   stop(paste("mu must be positive", "\n", ""))
-#' if (any(sigma*nu<=0))
-#'   stop(paste("Product sigma*nu must be positive", "\n", ""))
-#' if (log.p == TRUE)
-#'   p <- exp(p)
-#' else p <- p
-#' if (lower.tail == TRUE)
-#'   p <- p
-#' else p <- 1 - p
-#' if (any(p < 0) | any(p > 1))
-#'   stop(paste("p must be between 0 and 1", "\n", ""))
-#' q <- (1/mu)*(log1p( (p*(1-p)^(-1))^(1/nu) ))^(1/sigma)
-#' return(q)
-#' }
-#' rOW <- function(n, mu, sigma, nu){
-#' if(any(n<=0))
-#'   stop(paste("n must be positive","\n",""))
-#' if (any(mu<=0 ))
-#'   stop(paste("mu must be positive", "\n", ""))
-#' if (any(sigma*nu<=0))
-#'   stop(paste("Product sigma*nu must be positive", "\n", ""))
-#' n <- ceiling(n)
-#' p <- runif(n)
-#' r <- qOW(p, mu, sigma, nu)
-#' return(r)
-#' }
-#' x <- runif(200, 0, 10)
-#' nu <- 0.1 + 0.1*x
-#' y <- rOW(n=200, mu=0.05, sigma=2, nu=nu)
+#' x <- runif(50, 0, 10)
+#' shape <- 0.1 + 0.1*x
+#' y <- rweibull(n = 50, shape = shape, scale = pi)
 #'
 #' partitions <- list(method='quantile-based',
 #'                    folds=5)
