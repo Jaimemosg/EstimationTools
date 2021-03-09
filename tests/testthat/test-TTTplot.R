4e0561b5fbde283a1ba95b82561964f4494589ed
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-09 17:48:45 -0500

    Graphic test fixed.

--- tests/testthat/test-TTTplot.R
+++ tests/testthat/test-TTTplot.R
@@ -1,8 +1,8 @@
 test_that("plot.empiricalTTT", {
 
   set.seed(123)
-  y <- rweibull(n=50, shape=2, scale=pi)
+  y <- rweibull(n=1000, shape=2, scale=pi)
   TTT_3 <- TTTE_Analytical(y ~ 1, scaled = FALSE)
-  vdiffr::expect_doppelganger("TTT plot", plot(TTT_3, type = "s",
+  vdiffr::expect_doppelganger("TTT plot", plot(TTT_3, type = "s", grid = TRUE,
                                                col = 3, lwd = 3))
 })
