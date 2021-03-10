91a93db4917a07378d98f07b0a542c086a034d23
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-10 00:21:28 -0500

    Plot tests refreshed.

--- tests/testthat/test-TTT_HazardShape.R
+++ tests/testthat/test-TTT_HazardShape.R
@@ -1,9 +1,9 @@
 test_that("plot.HazardShape", {
 
-  set.seed(123)
-  y <- rweibull(n = 1000, shape = 2.5, scale = pi)
+  set.seed(456)
+  y <- rweibull(n = 10000, shape = 2.5, scale = pi)
   my_initial_guess <- TTT_hazard_shape(formula = y ~ 1)
-  vdiffr::expect_doppelganger("TTT for Hazard", plot(my_initial_guess,
-                                        par_plot=list(mar=c(3.7,3.7,1,1.5),
-                                        mgp=c(2.5,1,0))))
+  TTTplot <- function() plot(my_initial_guess)
+  # testthat::skip()
+  vdiffr::expect_doppelganger("TTT-for-Hazard", TTTplot)
 })
--- tests/testthat/test-TTTplot.R
+++ tests/testthat/test-TTTplot.R
@@ -3,6 +3,8 @@ test_that("plot.empiricalTTT", {
   set.seed(123)
   y <- rweibull(n=1000, shape=2, scale=pi)
   TTT_3 <- TTTE_Analytical(y ~ 1, scaled = FALSE)
-  vdiffr::expect_doppelganger("TTT plot", plot(TTT_3, type = "s", grid = TRUE,
-                                               col = 3, lwd = 3))
+  empTTT <- function() plot(TTT_3, type = "s", grid = TRUE,
+                            col = 3, lwd = 3)
+  # testthat::skip()
+  vdiffr::expect_doppelganger("TTT-plot", empTTT)
 })
