95bcd63f8fe94aacbece6c146266ce5a893adc9e
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-16 20:04:30 -0500

    Documentation enhanced.

--- R/Cells-data.R
+++ R/Cells-data.R
@@ -1,6 +1,6 @@
 #' Reduction cells
 #'
-#' Times to failure (in days) of 20 aluminum reduction cells.
+#' Times to failure (in units of 1000 days) of 20 aluminum reduction cells.
 #'
 #' @format A data frame with 20 observations.
 #' @examples
@@ -9,4 +9,7 @@
 #' hist(reduction_cells$days, main="", xlab="Time (Days)")
 #' plot(reduction_cells$status, xlab = "Status", lty = 3, type="h")
 #' points(reduction_cells$status, pch = 16)
+#'
+#' #' @references
+#' \insertRef{Whitmore1983}{EstimationTools}
 "reduction_cells"
