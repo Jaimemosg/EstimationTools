16da5491f54fb9d2cd0236bc511a675306bee0c6
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-16 19:21:16 -0500

    New data set added.

---
+++ R/Cells-data.R
@@ -0,0 +1,12 @@
+#' Reduction cells
+#'
+#' Times to failure (in days) of 20 aluminum reduction cells.
+#'
+#' @format A data frame with 20 observations.
+#' @examples
+#' data(reduction_cells)
+#' par(mfrow = c(1,2))
+#' hist(reduction_cells$days, main="", xlab="Time (Days)")
+#' plot(reduction_cells$status, xlab = "Status", lty = 3, type="h")
+#' points(reduction_cells$status, pch = 16)
+"reduction_cells"
