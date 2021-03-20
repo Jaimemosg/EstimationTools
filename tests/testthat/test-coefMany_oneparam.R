9bb5f48c1b77552f68469bef4a8eece159d572d0
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-20 13:34:08 -0500

    Coef method covered.

---
+++ tests/testthat/test-coefMany_oneparam.R
@@ -0,0 +1,10 @@
+test_that("running 'coefMany' maxlogLreg", {
+
+  x <- rnorm(n = 10000, mean = 160, sd = 6)
+  theta_1 <- maxlogL(x = x, dist = 'dnorm',
+                     link = list(over = "sd", fun = "log_link"),
+                     fixed = list(mean = 160))
+
+  a <- coefMany(theta_1, parameter = 'sd')
+  expect_length( a, 1 )
+})
