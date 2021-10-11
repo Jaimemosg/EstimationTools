acf831cbc47f9d8d0cd302a3962f785a8f7c33a2
Jaime Mosquera Gutiérrez <37384444+Jaimemosg@users.noreply.github.com>
2021-10-10 19:35:56 -0500

    Breakline added in message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname){
-  initmessage1 <- "><<<<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version "
+  initmessage1 <- "\n\n><<<<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version "
   initmessage2 <- utils::packageDescription("EstimationTools")$Version
   initmessage3 <- "   >>>>>>>>>>>>>>>>>>>>>>>><
   Feel free to report bugs in https://github.com/Jaimemosg/EstimationTools/issues"
