62dcbb3538d82b7c32f233aa723d4e83c1203bde
Jaime Mosquera Gutiérrez <37384444+Jaimemosg@users.noreply.github.com>
2021-10-10 19:37:29 -0500

    Key functions from classical distance sampling added.

---
+++ R/key_functions.R
@@ -0,0 +1,126 @@
+#==============================================================================
+# Default key functions ------------------------------------------------------
+#==============================================================================
+#' @title Half normal key function
+#' @family key functions
+#'
+#' @encoding UTF-8
+#' @author Jaime Mosquera Gutiérrez, \email{jmosquerag@unal.edu.co}
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' This function provides the half normal key function for model fitting in
+#' distance sampling.
+#'
+#' @param x vector of perpendicular distances from the transect.
+#' @param sigma scale parameter.
+#'
+#' @details
+#' This is the half normal key function with parameter
+#' \code{sigma}. Its expression is given by
+#'
+#' \eqn{g(x) = \exp(\frac{-x^2}{2*\sigma^2},}
+#'
+#' for x > 0.
+#'
+#' @return A numeric value corresponding to a given value of \code{x} and
+#' \code{sigma}.
+#'
+#' @examples
+#' library(EstimationTools)
+#'
+#' #----------------------------------------------------------------------------
+#' # Example: Half normal function
+#' half_norm(x=1, sigma=4.1058)
+#' curve(half_norm(x, sigma=4.1058), from=0, to=20, ylab='g(x)')
+#'
+#' #----------------------------------------------------------------------------
+#' @export
+half_norm <- function(x, sigma){
+  key <- exp(-x^2 / (2*sigma^2))
+  return(key)
+}
+#=============================================================================#
+#' @title Uniform key function
+#' @family key functions
+#'
+#' @encoding UTF-8
+#' @author Jaime Mosquera Gutiérrez, \email{jmosquerag@unal.edu.co}
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' This function provides the uniform key function for model fitting in
+#' distance sampling.
+#'
+#' @param x vector of perpendicular distances from the transect.
+#' @param w half width of the strip transect.
+#'
+#' @details
+#' This is the uniform key function with parameter
+#' \code{sigma}. Its expression is given by
+#'
+#' \eqn{g(x) = \frac{1}{w},}
+#'
+#' for x > 0.
+#'
+#' @return A numeric value corresponding to a given value of \code{x} and
+#' \code{w}.
+#'
+#' @examples
+#' library(EstimationTools)
+#'
+#' #----------------------------------------------------------------------------
+#' # Example: Uniform function
+#' uniform(x=1, w=100)
+#' curve(uniform(x, w=100), from=0, to=10, ylab='g(x)')
+#'
+#' #----------------------------------------------------------------------------
+#' @export
+uniform <- function(x, w){
+  key <- rep(1/w, length(x))
+  return(key)
+}
+#=============================================================================#
+#' @title Hazard rate key function
+#' @family key functions
+#'
+#' @encoding UTF-8
+#' @author Jaime Mosquera Gutiérrez, \email{jmosquerag@unal.edu.co}
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' This function provides the hazard rate key function for model fitting in
+#' distance sampling.
+#'
+#' @param x vector of perpendicular distances from the transect.
+#' @param sigma scale parameter.
+#' @param beta shape parameter.
+#'
+#' @details
+#' This is the hazard rate key function with parameters
+#' \code{sigma} and \code{beta}. Its expression is given by
+#'
+#' \eqn{g(x) = 1 - \exp((\frac{-x}{\sigma})^{-\beta},}
+#'
+#' for x > 0.
+#'
+#' @return A numeric value corresponding to a given value of \code{x},
+#' \code{sigma} and \code{beta}.
+#'
+#' @examples
+#' library(EstimationTools)
+#'
+#' #----------------------------------------------------------------------------
+#' # Example: Hazard rate function
+#' uniform(x=1, w=100)
+#' curve(uniform(x, w=100), from=0, to=10, ylab='g(x)')
+#'
+#' #----------------------------------------------------------------------------
+#' @export
+hazard_rate <- function(x, sigma, beta){
+  key <- 1 - exp(-(x / sigma)^(-beta))
+  return(key)
+}
