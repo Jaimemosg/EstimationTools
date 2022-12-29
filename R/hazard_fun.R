fbad1938ff6ad9229b55883531b7bd97b164572a
Jaimemosg <jmosquerag@unal.edu.co>
2022-12-28 22:29:39 -0500

    Drop unnecessary argument (support).

--- R/hazard_fun.R
+++ R/hazard_fun.R
@@ -12,15 +12,8 @@
 #'
 #' @param distr a length-one character vector with the name of density/mass function
 #'              of interest.
-#' @param support a list with the following entries:
-#'                \itemize{
-#'                \item \code{interval}: a two dimensional atomic vector indicating the
-#'                set of possible values of a random variable having the
-#'                distribution specified in \code{y_dist}.
-#'                \item \code{type}: character indicating if distribution has a
-#'                \code{discrete} or a \code{continous} random variable.
-#'                }
-#' @param method
+#' @param log logical; if TRUE, the natural logarithm of the hazard values are
+#'            returned.
 #'
 #' @return A function with the folling input arguments:
 #' \item{x}{vector of (non-negative) quantiles.}
@@ -31,10 +24,9 @@
 #'
 #' #--------------------------------------------------------------------------------
 #' # Example 1: Hazard function of the Weibull distribution.
-#' support <- list(interval=c(0, Inf), type='continuous')
 #'
 #' # Hazard function in the 'maxlogL' framework
-#' hweibull1 <- hazard_fun('dweibull', support)
+#' hweibull1 <- hazard_fun('dweibull')
 #'
 #' # Hazard function from scratch
 #' hweibull2 <- function(x, shape, scale){
@@ -51,26 +43,26 @@
 #' @export
 hazard_fun <-  function(
     distr,
-    support,
+    # support,
     log = FALSE
 ){
-  type <- match.arg(support$type, c("continuous", "discrete"))
-
   hfun <- function(x, ...){
     pdf <- do.call(what = distr,
                    args = c(list(x = x, log = log), ...))
     sf <- do.call(what = paste0('p', substring(distr, 2)),
                   args = c(list(q = x, log.p = log, lower.tail = FALSE), ...))
     hf <- if (log){ pdf - cdf } else { pdf/sf }
-    hf <- 'empty'
+    # hf <- 'empty'
     return(hf)
   }
 
-  if (type == 'discrete'){
-    body(hfun)[[5]] <- quote(hf <- ifelse(hf > 1, 1, hf))
-  } else {
-    body(hfun)[[5]] <- NULL
-  }
+  # type <- match.arg(support$type, c("continuous", "discrete"))
+  #
+  # if (type == 'discrete'){
+  #   body(hfun)[[5]] <- quote(hf <- ifelse(hf > 1, 1, hf))
+  # } else {
+  #   body(hfun)[[5]] <- NULL
+  # }
   return(hfun)
 }
 
@@ -96,6 +88,13 @@ hazard_fun <-  function(
 #'                \item \code{type}: character indicating if distribution has a
 #'                \code{discrete} or a \code{continous} random variable.
 #'                }
+#' @param method a character or function; if \code{"log_sf"}, the cumulative
+#'               hazard function (CHF) is computed using the expression
+#'               \eqn{H(t) = -\log (S(t))}; if \code{"integrate_hf"}, the CHF is
+#'               computed with the integral of the hazard function.
+#' @param routine a character specifying the integration routine.
+#'                \code{integrate} and \code{gauss_quad} are available, but the
+#'                custom routines can be defined.
 #'
 #' @return A function with the folling input arguments:
 #' \item{x}{vector of (non-negative) quantiles.}
@@ -112,7 +111,7 @@ hazard_fun <-  function(
 #' Hweibull1 <- cum_hazard_fun(
 #'   distr = 'dweibull',
 #'   support = support,
-#'   method = "integrate_hf"
+#'   method = "integration"
 #'  )
 #'
 #'  Hweibull2 <- cum_hazard_fun(
@@ -122,7 +121,7 @@ hazard_fun <-  function(
 #'  )
 #'
 #' # Compute cumulative hazard function from scratch
-#' # Recall h(x) = shape/scale * (x/scale)^(shape - 1)
+#' # Recall h(x) = shape/scale * (x/scale)^(shape - 1), then H(x) = (x/scale)^shape
 #'
 #' Hweibull3 <- function(x, scale, shape){
 #'   (x/scale)^shape
@@ -140,7 +139,8 @@ hazard_fun <-  function(
 cum_hazard_fun <- function(
     distr,
     support = NULL,
-    method = c("log_sf", "integrate_hf")
+    method = c("log_sf", "integration"),
+    routine = NULL
 ){
   if (!is.null(support)){
     type <- match.arg(support$type, c("continuous", "discrete"))
@@ -148,7 +148,20 @@ cum_hazard_fun <- function(
 
   method <- match.arg(method)
 
-  Hfun <- cumhazfun_method[[method]](distr, support)
+  if (method == "log_sf"){
+    Hfun <- cumhazfun_method[[method]](distr)
+  }
+
+  if (method == "integration"){
+    if (is.null(routine)){
+      if (support$type == 'continuous'){
+        routine <- 'integrate'
+      } else if (support$type == 'discrete'){
+        routine <- 'summate'
+      }
+    }
+    Hfun <- cumhazfun_method[[method]](distr, support, routine)
+  }
   return(Hfun)
 }
 #==============================================================================
@@ -166,7 +179,7 @@ cumhf_log_sf <- function(distr, support){
   return(Hfun)
 }
 
-cumhf_integral <- function(distr, support){
+cumhf_integral <- function(distr, support, routine){
   if (is.null(support)) stop(
     paste0(
       "Please, provide the support of the distribution as a list with the ",
@@ -174,16 +187,11 @@ cumhf_integral <- function(distr, support){
       "elements vector.")
   )
   Hfun <- function(x, ...){
-    integrand <- hazard_fun(distr, support)
+    integrand <- hazard_fun(distr)
 
-    if (support$type == 'continuous'){
-      routine <- 'integrate'
-    } else if (support$type == 'discrete'){
-      routine <- 'summate'
-    }
-
-    Hf <- do.call(what = 'integration',
-                  args = c(list(fun = integrand, lower = support$interval[1],
+    Hf <- do.call(what = "integration",
+                  args = c(list(fun = integrand,
+                                lower = support$interval[1],
                                 upper = x,
                                 routine = routine, ...)))
     return(Hf)
@@ -193,5 +201,5 @@ cumhf_integral <- function(distr, support){
 
 cumhazfun_method <- list(
   "log_sf" = cumhf_log_sf,
-  "integrate_hf" = cumhf_integral
+  "integration" = cumhf_integral
 )
