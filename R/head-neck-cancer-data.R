efdc7bbe15e874aab8f85fda33ba26e99663ba25
Jaimemosg <jmosquerag@unal.edu.co>
2023-02-04 21:07:51 -0500

    Add head and neck cancer data set

---
+++ R/head-neck-cancer-data.R
@@ -0,0 +1,23 @@
+#' Head and neck cancer
+#'
+#' Time-to-event data a randomized clinical trial to compare two therapies for
+#' head and neck cancer.51 patients were treated with radiation only and 45
+#' patients treated with radiation plus chemotherapy. The variables are:
+#'
+#' \itemize{
+#'   \item Time: time (in days) to recurrence of the cancer.
+#'   \item Therapy: treatment applied to the patients.
+#'   \item Status: censorship indicators.
+#' }
+#'
+#' @format A data frame with 96 observations.
+#' @examples
+#' data(head_neck_cancer)
+#' par(mfrow = c(1,2))
+#' hist(head_neck_cancer$Time, main="", xlab="Time (Days)")
+#' plot(head_neck_cancer$Therapy, xlab = "Status", lty = 3, type="h")
+#' points(head_neck_cancer$status, pch = 16)
+#'
+#' @references
+#' \insertRef{Khan2018}{EstimationTools}
+"head_neck_cancer"
