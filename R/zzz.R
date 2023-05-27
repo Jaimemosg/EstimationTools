8e4a8d79a8aaa743a07aeaf95eec88996aa27082
Jaimemosg <jmosquerag@unal.edu.co>
2023-05-27 12:52:55 -0500

    Hide startup messages from dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,9 @@
 .onAttach <- function(libname, pkgname){
-  initmessage1 <- "\n\n><<<<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version "
+  invisible(suppressPackageStartupMessages(
+    sapply(c("GA", "DEoptim", "BBmisc", "survival"),
+           requireNamespace, quietly = TRUE)
+  ))
+  initmessage1 <- "\n><<<<<<<<<<<<<<<<<<<<<<<<   EstimationTools Version "
   initmessage2 <- utils::packageDescription("EstimationTools")$Version
   initmessage3 <- "   >>>>>>>>>>>>>>>>>>>>>>>><
   Feel free to report bugs in https://github.com/Jaimemosg/EstimationTools/issues"
