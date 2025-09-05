1ae7c48d172d29699c2be56b889fe1b44cfce1ff
Jaimemosg <jmosquerag@unal.edu.co>
2025-09-05 15:30:14 -0500

    Skip spelling during coverage

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,6 @@
-if(requireNamespace('spelling', quietly = TRUE))
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE,
-                             skip_on_cran = TRUE)
+# tests/spelling.R
+if (Sys.getenv("R_COVR") == "true") {
+  cat("Skipping spelling::spell_check_test() under covr.\n")
+} else if (requireNamespace("spelling", quietly = TRUE)) {
+  spelling::spell_check_test(vignettes = TRUE, error = FALSE, skip_on_cran = TRUE)
+}
